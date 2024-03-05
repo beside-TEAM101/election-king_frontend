5c9ab8a1058994c05e4b7e0502070fa23c1afc73
parksol <thfdl0317@naver.com>
2024-03-05 21:14:27 +0900

    style : scss적용, 색상추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import styles from "./page.module.css";
+import variables from "@/app/assets/styles/variables.module.scss";
 
 export default function Home() {
-  return <main className={styles.main}>총선</main>;
+  return (
+    <div>
+      <h1 className={styles.main}>총선</h1>
+      <p className={variables.title}> 총선 scss적용</p>
+    </div>
+  );
 }
