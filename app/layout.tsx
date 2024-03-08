726923563572bad50fc9097686095b0e8d80fdcd
parksol <thfdl0317@naver.com>
2024-03-08 23:24:25 +0900

    chore: Prettier, 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from "next";
-import PreFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next'
+import PreFont from 'next/font/local'
+import './globals.css'
 
 const pretendard = PreFont({
-  src: "../public/fonts/Pretendard-Regular.woff2",
-  display: "swap",
-});
+	src: '../public/fonts/Pretendard-Regular.woff2',
+	display: 'swap',
+})
 
 export const metadata: Metadata = {
-  title: "총선 서비스",
-  description: "유권자들이 후보자들의 정보를 간편 조회할 수 있는 서비스",
-};
+	title: '총선 서비스',
+	description: '유권자들이 후보자들의 정보를 간편 조회할 수 있는 서비스',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="ko">
-      <body className={pretendard.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="ko">
+			<body className={pretendard.className}>{children}</body>
+		</html>
+	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import styles from "./page.module.css";
-import variables from "@/app/assets/styles/variables.module.scss";
+import variables from '@/app/assets/styles/variables.module.scss'
+import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className={styles.main}>총선</h1>
-      <p className={variables.title}> 총선 scss적용</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className={styles.main}>총선</h1>
+			<p className={variables.title}> 총선 scss적용</p>
+		</div>
+	)
 }
