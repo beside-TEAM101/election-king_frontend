12270e783527b0f0086f42821863309f4179c8a1
go-lani <go.lani.developer@gmail.com>
2024-03-26 00:30:24 +0900

    fix: 의안 더보기 수정

--- app/(candidate)/detail/[id]/page.tsx
+++ app/(candidate)/detail/[id]/page.tsx
@@ -138,6 +138,7 @@ export default async function Detail({ params }: { params: { id: string } }) {
 							<BillVotingList
 								candidateName={detail?.name}
 								billVoteList={billVoteList}
+								monaCode={detail?.monaCode}
 							/>
 						) : (
 							<div className={detailStyle.voting_empty}>
