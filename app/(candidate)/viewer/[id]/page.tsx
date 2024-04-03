ca5e1112a66d6963af1d0d345232678bdc074c70
이철환 <go.lani.developer@gmail.com>
2024-04-03 23:34:27 +0900

    fix: build 에러

--- app/(candidate)/viewer/[id]/page.tsx
+++ app/(candidate)/viewer/[id]/page.tsx
@@ -3,5 +3,6 @@
 import PDFViewer from '@/components/common/PDFViewer'
 
 export default function Viewer({ params }: { params: { id: string } }) {
+	console.log('params', params)
 	return <PDFViewer />
 }
