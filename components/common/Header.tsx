1fb27738aafbdd2e765f5a9a14ee171bdc65285c
parksol <thfdl0317@naver.com>
2024-04-05 02:06:13 +0900

    add : 후보자 정보 및 목록, 서비스를 공유할 수 있는 기능

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,12 +3,18 @@
 import Image from 'next/image'
 import goBackBtn from '@/public/assets/icons/back-arrow.svg'
 import goMainBtn from '@/public/assets/icons/home.svg'
+import shareBtn from '@/public/assets/icons/share.svg'
 import variables from '@/styles/variables.module.scss'
 import Link from 'next/link'
 import useRoute from '@/hooks/useRoute'
+import { useEffect, useState } from 'react'
 
 function Header() {
-	const { router, pathname } = useRoute()
+	const { router, pathname, queries } = useRoute()
+	const { city, district } = queries.query
+
+	const [isShareSupported, setIsShareSupported] = useState(false)
+
 	const goBack = () => {
 		if (pathname === '/list') {
 			router.push('/')
@@ -17,19 +23,59 @@ function Header() {
 
 		router.back()
 	}
+
+	const handleListShare = async () => {
+		if (isShareSupported) {
+			try {
+				await navigator.share({
+					title: '뽑기왕',
+					text: '뽑아라! 22대 총선, 당신의 한 표로 결과가 바뀐다!',
+					url: `http://localhost:3000/list?candidates=&pageIndex=0&pageSize=50&type=CONGRESS&city=${city}&district=${district}`,
+				})
+				console.log('성공')
+			} catch (e) {
+				console.log('실패')
+			}
+		} else {
+			console.log('test')
+		}
+	}
+
+	useEffect(() => {
+		const checkShareSupport = () => {
+			if (navigator.share) {
+				setIsShareSupported(true)
+			} else {
+				setIsShareSupported(false)
+			}
+		}
+		checkShareSupport()
+	}, [])
+
 	return (
 		<div className={variables.header}>
 			<button type="button" onClick={goBack}>
 				<Image src={goBackBtn} alt="뒤로가기 아이콘" width={24} height={24} />
 			</button>
-			<Link href="/">
-				<Image
-					src={goMainBtn}
-					alt="메인페이지로 가는 아이콘"
-					width={24}
-					height={24}
-				/>
-			</Link>
+
+			<div>
+				{isShareSupported && (
+					<button
+						type="button"
+						className={variables.shareBtn}
+						onClick={handleListShare}>
+						<Image src={shareBtn} alt="공유 아이콘" width={24} height={24} />
+					</button>
+				)}
+				<Link href="/">
+					<Image
+						src={goMainBtn}
+						alt="메인페이지로 가는 아이콘"
+						width={24}
+						height={24}
+					/>
+				</Link>
+			</div>
 		</div>
 	)
 }
